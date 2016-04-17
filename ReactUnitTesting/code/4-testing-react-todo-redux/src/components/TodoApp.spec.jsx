c6320796b36f9b376f23dd33c0586a46161a2784
Tom Duncalf <tom@tomduncalf.com>
2016-04-17 20:25:08 +0100

    Redux goodness

--- ReactUnitTesting/code/4-testing-react-todo-redux/src/components/TodoApp.spec.jsx
+++ ReactUnitTesting/code/4-testing-react-todo-redux/src/components/TodoApp.spec.jsx
@@ -7,6 +7,7 @@ import { default as DecoratedTodoApp, TodoApp } from './TodoApp'
 import TodoList from './TodoList'
 import TodoInput from './TodoInput'
 import { default as todosReducer } from 'redux/todos'
+import * as todosActions from 'redux/todos'
 
 function addItem(wrapper, item) {
     wrapper.find('input').get(0).value = item
@@ -23,6 +24,17 @@ function assertItems(wrapper, items) {
     })
 }
 
+function getMountedAndStore(initialState = undefined) {
+    const store = createStore(todosReducer, initialState)
+    const context = { store }
+
+    return [mount(<DecoratedTodoApp />, { context }), store]
+}
+
+function getMounted(initialState = undefined) {
+    return getMountedAndStore(initialState)[0]
+}
+
 describe('TodoApp (un-decorated)', () => {
     describe('rendering', () => {
         it('should render correctly', () => {
@@ -33,11 +45,6 @@ describe('TodoApp (un-decorated)', () => {
         })
     })
 })
-
-function getMounted() {
-    const context = { store: createStore(todosReducer) }
-    return mount(<DecoratedTodoApp />, { context })
-}
 
 describe('TodoApp (decorated)', () => {
     describe('adding items (using mount)', () => {
@@ -62,5 +69,42 @@ describe('TodoApp (decorated)', () => {
 
             assertItems(wrapper, [{ item: 'test 1', done: true }, { item: 'test 2', done: false }])
         })
+    })
+})
+
+describe('TodoApp (Redux integration)', () => {
+    it('should render items from the initial state', () => {
+        const initialState = {
+            todos: [
+                { item: 'test 1', done: true },
+                { item: 'test 2' },
+            ]
+        }
+        const wrapper = getMounted(initialState)
+
+        assertItems(wrapper, [{ item: 'test 1', done: true }, { item: 'test 2', done: false }])
+    })
+
+    it('should add items to a list when calling the addTodo action', () => {
+        const [wrapper, store] = getMountedAndStore()
+
+        store.dispatch(todosActions.addTodo({ item: 'test 1' }))
+        store.dispatch(todosActions.addTodo({ item: 'test 2' }))
+
+        assertItems(wrapper, [{ item: 'test 1' }, { item: 'test 2' }])
+    })
+
+    it('should mark items as done when calling the toggleTodoDone action', () => {
+        const initialState = {
+            todos: [
+                { item: 'test 1', done: true },
+                { item: 'test 2' },
+            ]
+        }
+        const [wrapper, store] = getMountedAndStore(initialState)
+
+        store.dispatch(todosActions.toggleTodoDone(1))
+
+        assertItems(wrapper, [{ item: 'test 1', done: true }, { item: 'test 2', done: true }])
     })
 })
