886afdd1ddb7f9297a44804e73f9e42616bbd3f5
Tom Duncalf <tom@tomduncalf.com>
2016-04-17 18:32:18 +0100

    Integration tests

--- ReactUnitTesting/code/3-testing-react-todo/src/components/TodoApp.spec.jsx
+++ ReactUnitTesting/code/3-testing-react-todo/src/components/TodoApp.spec.jsx
@@ -12,6 +12,15 @@ function addItem(wrapper, item) {
     wrapper.find('form').simulate('submit')
 }
 
+function assertItems(wrapper, items) {
+    expect(wrapper).to.have.exactly(items.length).descendants('li')
+    items.forEach((item, index) => {
+        const itemLi = wrapper.find('li').at(index)
+        expect(itemLi).to.contain.text(item.item)
+        expect(itemLi).to.contain.text(item.done ? 'Not done' : 'Done')
+    })
+}
+
 describe('TodoApp', () => {
     describe('rendering', () => {
         it('should render correctly', () => {
@@ -47,13 +56,26 @@ describe('TodoApp', () => {
     })
 
     describe('adding items (using mount)', () => {
-        it('should add an item to an empty list', () => {
+        it('should add items to a list', () => {
             const wrapper = mount(<TodoApp />)
-            // addItem(wrapper, 'test')
-            wrapper.find('input').get(0).value = 'Test'
-            wrapper.find('input').simulate('change')
-            wrapper.find('form').simulate('submit')
-            //expect(wrapper).to.have.exactly(1).descendants('li')
+
+            addItem(wrapper, 'test 1')
+            assertItems(wrapper, [{ item: 'test 1', done: false }])
+
+            addItem(wrapper, 'test 2')
+            assertItems(wrapper, [{ item: 'test 1', done: false }, { item: 'test 2', done: false }])
+        })
+    })
+
+    describe('marking items done (using mount)', () => {
+        it('should mark an item as done', () => {
+            const wrapper = mount(<TodoApp />)
+
+            addItem(wrapper, 'test 1')
+            addItem(wrapper, 'test 2')
+            wrapper.find('li').at(0).find('a').simulate('click')
+
+            assertItems(wrapper, [{ item: 'test 1', done: true }, { item: 'test 2', done: false }])
         })
     })
 })
