c7e2d7bf1dd4ad4f7fc7fad2a23d4d46c3ed33aa
Tom Duncalf <tom@tomduncalf.com>
2016-04-18 19:32:34 +0100

    WIP

--- ReactUnitTesting/code/1-testing-javascript/1-simple.js
+++ ReactUnitTesting/code/1-testing-javascript/1-simple.js
@@ -1,8 +1,13 @@
-const assert = require('assert')
 const add = require('./add')
 
 describe('add function', () => {
     it('should add two numbers together correctly', () => {
-        assert(add(1, 2) === 3)
+        if (add(1, 2) !== 3) {
+            throw new Error('1 + 2 did not equal 3')
+        }
+
+        // or...
+        const assert = require('assert')
+        assert(add(1, 2) === 3, '1 + 2 did not equal 3')
     })
 })
--- ReactUnitTesting/code/1-testing-javascript/1a-failures.js
+++ ReactUnitTesting/code/1-testing-javascript/1a-failures.js
@@ -3,6 +3,6 @@ const add = require('./add')
 
 describe('add function', () => {
     it('should add three numbers together correctly', () => {
-        assert(add(1, 2, 3) === 6)
+        assert(add(1, 2, 3) === 6, '1 + 2 + 3 did not equal 6')
     })
 })
